b7b041377998ddf469fdc4e0037baad1cbf5febe
Ashish Vaid <vaid50@yahoo.com>
2023-02-21 19:05:35 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ function requestHandler(req, res) {
 
 const server = http.createServer(requestHandler);
 const port = process.env.PORT || 8888;
-server.listen(port, () => console.log(`listening on port ${port}`));
+server.listen(port, () => console.log(`cloud run listening on port ${port}`));
